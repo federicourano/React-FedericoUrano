90697dbdc4ac3a73ea594de52e39c7763b01aeac
Federico Urano <federicourano2013@gmail.com>
2024-03-28 21:36:04 -0300

    Agrego cosas de la clase 10

--- ecommerce-urano/src/components/ItemDetail/ItemDetail.jsx
+++ ecommerce-urano/src/components/ItemDetail/ItemDetail.jsx
@@ -1,7 +1,22 @@
 import ItemCount from "../ItemCount/ItemCount"
 import clase from "./ItemDetail.module.css"
+import { useState } from "react"
+import { Link } from "react-router-dom"
 
 const ItemDetail = ({ id, name, img, category, price, descripcion, seller, stock }) => {
+
+    const [quantity, setQuantity] = useState(0)
+
+    const handleOnAdd = (count) => {
+        const objProductToAdd = {
+            id, name, price, count
+        }
+
+        setQuantity(count)
+        
+
+    }
+
     return(
         <div className={clase.divpapa}>
             <div className={clase.divimagen}>
@@ -13,7 +28,15 @@ const ItemDetail = ({ id, name, img, category, price, descripcion, seller, stock
                 <h4 className= {clase.descripcion}>Descripción: {descripcion}</h4>
                 <h3>Fabricante: {seller}</h3>
                 <h3>Precio: ${price}</h3>
-                <ItemCount stock={stock}/>
+                <div>
+                    {
+                        quantity === 0 ? (
+                            <ItemCount onAdd={handleOnAdd} stock={stock}/>
+                        ) : (
+                            <Link to='/cart'>Finalizar compra</Link>
+                        )
+                    }    
+                </div>
             </div>
         </div>
     )
